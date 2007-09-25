a9f5830a297a923dd30e0dc4e5e2d6c31d2e3df1
Roger Ineichen <roger@projekt01.ch>
2007-09-25 01:44:33 +0000

    Prepare for 3.4 egg

---
+++ README.txt
@@ -0,0 +1,1 @@
+This package provides application setup helpers.
--- setup.py
+++ setup.py
@@ -17,31 +17,52 @@ $Id$
 """
 
 import os
-from setuptools import setup, find_packages, Extension
+
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='zope.app.appsetup',
-      version = '3.4.0b1',
-      url='http://svn.zope.org/zope.app.appsetup',
-      license='ZPL 2.1',
-      author='Zope Corporation and Contributors',
-      author_email='zope3-dev@zope.org',
-	  packages=find_packages('src'),
-	  package_dir = {'': 'src'},
-      extras_require=dict(test=['zope.app.testing']),
-      namespace_packages=['zope', 'zope.app'],
-      install_requires=['setuptools',
-                        'zope.interface',
-                        'zope.security',
-                        'zope.component',
-                        'zope.app.component',
-                        'zope.app.folder',
-                        'zope.configuration',
-                        'zope.event',
-                        'zope.traversing',
-                        'zope.app.container',
-                        'zope.app.publication',
-                        'ZODB3',
-                        ],
-      include_package_data = True,
-      zip_safe = False,
-      )
+    version = '3.4.0',
+    author='Zope Corporation and Contributors',
+    author_email='zope3-dev@zope.org',
+    description = "Zope app setup helper",
+    long_description=(
+        read('README.txt')
+        + '\n\n' +
+        read('CHANGES.txt')
+        ),
+    license='ZPL 2.1',
+    keywords = "zope3 app setup",
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Programming Language :: Python',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Framework :: Zope3'],
+    url='http://cheeseshop.python.org/pypi/zope.app.appsetup',
+    packages=find_packages('src'),
+    package_dir = {'': 'src'},
+    extras_require=dict(test=['zope.app.testing']),
+    namespace_packages=['zope', 'zope.app'],
+    install_requires=['setuptools',
+                      'zope.interface',
+                      'zope.security',
+                      'zope.component',
+                      'zope.app.component',
+                      'zope.app.folder',
+                      'zope.configuration',
+                      'zope.event',
+                      'zope.traversing',
+                      'zope.app.container',
+                      'zope.app.publication',
+                      'ZODB3',
+                      ],
+    include_package_data = True,
+    zip_safe = False,
+    )
