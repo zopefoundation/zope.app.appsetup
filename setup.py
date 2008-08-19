c57ec712860596ad06ca650bc1d854169e8b637c
Fred Drake <fdrake@acm.org>
2008-08-19 16:48:21 +0000

    fix version number for release

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def read(*rnames):
 
 setup(
     name='zope.app.appsetup',
-    version='3.7.1dev',
+    version='3.7.0',
     author='Zope Corporation and Contributors',
     author_email='zope3-dev@zope.org',
     description="Zope app setup helper",
