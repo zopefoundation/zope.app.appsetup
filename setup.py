fc8d927cadd41d5d3d927c4e7f767151ccf86667
Stephan Richter <stephan.richter@gmail.com>
2008-08-25 22:42:07 +0000

    Get ready for release.

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def read(*rnames):
 
 setup(
     name='zope.app.appsetup',
-    version='3.7.1dev',
+    version='3.8.0',
     author='Zope Corporation and Contributors',
     author_email='zope-dev@zope.org',
     description="Zope app setup helper",
