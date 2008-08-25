295b26df6a4d380dcd6b3236184deffc4a4f3112
Stephan Richter <stephan.richter@gmail.com>
2008-08-25 21:47:41 +0000

    Fix E-mail address too.

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     name='zope.app.appsetup',
     version='3.7.1dev',
     author='Zope Corporation and Contributors',
-    author_email='zope3-dev@zope.org',
+    author_email='zope-dev@zope.org',
     description="Zope app setup helper",
     long_description=(
         read('README.txt')
