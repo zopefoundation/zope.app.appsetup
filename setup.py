c32fc14efc90bde07192df8857e47079a2e62d1c
Christian Theune <ct@gocept.com>
2011-01-27 14:12:46 +0000

    Preparing release 3.16.0

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ def read(*rnames):
 
 setup(
     name='zope.app.appsetup',
-    version='3.16.0dev',
+    version='3.16.0',
     author='Zope Corporation and Contributors',
     author_email='zope-dev@zope.org',
     description="Zope app setup helper",
