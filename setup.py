bcf1cb7fea30915d3b5a660743221e9d852ddd25
Michael Howitz <mh@gocept.com>
2016-07-06 11:00:26 +0200

    Claim support for Python 3.4 and 3.5.

--- setup.py
+++ setup.py
@@ -55,6 +55,8 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Natural Language :: English',
         'Operating System :: OS Independent',
         'Topic :: Internet :: WWW/HTTP',
