ca7274d00cef06de0f31fc2ef34747c04d9f397d
Michael Howitz <mh@gocept.com>
2018-12-15 13:30:12 +0100

    Preparing release 4.1.0

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ def read(*rnames):
 
 setup(
     name='zope.app.appsetup',
-    version='4.1.0.dev0',
+    version='4.1.0',
     author='Zope Corporation and Contributors',
     author_email='zope-dev@zope.org',
     description="Zope app setup helper",
